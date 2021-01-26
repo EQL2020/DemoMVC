8ac731c744142476c9b10169f42f13db283598e2
eqlrepository <eqlrepository@gmail.com>
2021-01-26 10:19:37 +0100

    Ajoutez des fichiers projet.

---
+++ DemoMVC/Controllers/PersonneController.cs
@@ -0,0 +1,43 @@
+﻿using DemoMVC.DAO;
+using DemoMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoMVC.Controllers
+{
+    public class PersonneController : Controller
+    {
+        public IActionResult Ajouter()
+        {
+            return View();
+        }
+
+        public IActionResult Liste()
+        {
+            List<Personne> liste = new PersonneDAO().GetAll();
+
+            ViewBag.Personnes = liste;
+
+            return View();
+        }
+
+        [HttpPost] // je spécifie que cette action correspond au post
+        public IActionResult Ajouter(string txtNom, string txtPrenom)
+        {
+            // j'enregistre les infos dans la base....
+            Personne p = new Personne();
+            p.Nom = txtNom;
+            p.Prenom = txtPrenom;
+
+            PersonneDAO persDao = new PersonneDAO();
+            persDao.Inserer(p);
+
+            // rediriger vers la liste
+            return RedirectToAction("Liste");
+
+        }
+    }
+}
